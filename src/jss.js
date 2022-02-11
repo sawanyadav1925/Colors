eab6ab2619f6475a42edca8efad30362971022ca
sawanyadav1925 <sawanyadav@cedcoss.com>
2022-02-11 19:02:47 +0530

    color

---
+++ src/jss.js
@@ -0,0 +1,20 @@
+function box1(){
+
+    document.body.style.backgroundColor = "#FE657D";
+}
+function box2(){
+
+    document.body.style.backgroundColor = "#FCB2A9";
+}
+function box3(){
+
+    document.body.style.backgroundColor = "#E3E3DB";
+}
+function box4(){
+
+    document.body.style.backgroundColor = "94E4C9";
+}
+function box5(){
+
+    document.body.style.backgroundColor = "#509B7D";
+}
